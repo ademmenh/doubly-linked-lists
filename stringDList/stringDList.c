69a91d295e97605e1a719fd161f53de741126cd1
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-09-24 23:31:59 +0100

    new function
    funcstringDNodePointer ()

--- stringDList/stringDList.c
+++ stringDList/stringDList.c
@@ -64,6 +64,46 @@ stringDNode* funcstringDNodeCreate (string value)
     return vpNew;
 }
 
+stringDNode* funcstringDNodePointer (stringDList DList, int index)
+{
+    stringDNode *vpCn;
+    int viCn;
+
+
+    if ( 0<=index )
+    {
+        if ( DList.length-1<index )
+        {
+            printf ("The index is out the range of the DList !!!");
+            exit (1);
+        }
+
+        vpCn = DList.H;
+        for ( viCn=0; viCn<index; viCn++ )
+        {
+            vpCn = vpCn->Next;
+        }
+
+        return vpCn;
+    }
+    else
+    {
+        if ( DList.length<-index )
+        {
+            printf ("The index is out the range of the DList !!!");
+            exit (1);
+        }
+
+        vpCn = DList.T;
+        for ( viCn=-1; index<viCn; viCn-- )
+        {
+            vpCn = vpCn->Previous;
+        }
+
+        return vpCn;
+    }
+}
+
 void funcstringDNodeFree (stringDNode *pDNode)
 {
     funcstringClear (&(pDNode->Value));
